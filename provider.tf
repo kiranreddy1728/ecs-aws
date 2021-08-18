f2a95e5046c1e96739abe3d85f592a8cd516dc39
kiranreddy1728 <30837279+kiranreddy1728@users.noreply.github.com>
2021-08-18 23:37:52 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,9 +1,9 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAZUUA62HOAZS4VWEJ"
-	secret_key = "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"
+    access_key = "XXXXXXXXXXX"
+	secret_key = "XXXXXXXXXXXX"
     /*assume_role {
-        role_arn     = "arn:aws:iam::662769160668:role/terraform"
+        role_arn     = "arn:aws:iam::XXXXXXX:role/terraform"
         session_name = "SESSION_NAME"
         external_id  = "EXTERNAL_ID"
   }*/
