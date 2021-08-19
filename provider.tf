62000d171475c25ae5a66eb1cddc77e67823f913
Kiran Bojjala <bojjala17@gmail.com>
2021-08-19 14:50:57 +0530

    creation of ecr code

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAZUUA62HOAZS4VWEJ"
-	secret_key = "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"
+    access_key = ""
+	secret_key = ""
     /*assume_role {
         role_arn     = "arn:aws:iam::662769160668:role/terraform"
         session_name = "SESSION_NAME"
