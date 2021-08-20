2811172bbec8616bffb9d7baa06b9a2eaab3ec51
Kiran Bojjala <bojjala17@gmail.com>
2021-08-20 21:25:21 +0530

    creation of ecr code

--- roles.tf
+++ roles.tf
@@ -1,3 +1,40 @@
+resource "aws_iam_user" "example" {
+  name          = "kiran"
+  path          = "/"
+  force_destroy = true
+
+
+}
+
+# Attach custom or managed policies
+resource "aws_iam_user_policy_attachment" "policy" {
+
+  user       = aws_iam_user.example.name
+  count      = length(var.iam_policy_arn)
+  policy_arn = var.iam_policy_arn[count.index]
+
+}
+
+# Define policy ARNs as list
+variable "iam_policy_arn" {
+  description = "IAM Policy to be attached to role"
+  type = list
+  default = [ "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser", 
+              "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+              "arn:aws:iam::aws:policy/ElasticLoadBalancingReadOnly",
+              "arn:aws:iam::aws:policy/AmazonECS_FullAccess"]
+}
+
+
+
+
+
+# resource "aws_iam_access_key" "lb" {
+#   user    = aws_iam_user.example.name
+#   pgp_key = "keybase:test"
+  
+# }
+
 # # ECS task execution role data
 # data "aws_iam_policy_document" "ecs_task_execution_role" {
 #   version = "2012-10-17"
